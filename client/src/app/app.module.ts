b16a8461ab0a2206077cd280a560ddb811533d02
Jeremy Wilson <jwilson@meritmile.com>
2017-09-21 17:24:48 -0400

    At the add blog stage all good so far

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
+import { NotAuthGuard } from './guards/notAuth.guard';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, NotAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+// Database Connection
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
   if (err) {
@@ -13,12 +18,19 @@ mongoose.connect(config.uri, (err) => {
   }
 });
 
-app.use(express.static(__dirname + '/client/dist/'));
+// Middleware
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use('/authentication', authentication);
 
+// Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
+// Start Server: Listen on port 8080
 app.listen(8080, () => {
   console.log('listening on port 8080');
 });
